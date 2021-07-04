26be0d94733c24a72e7ddba5ed0416b002075646
Ajay Jangid <ajayjangid9299@gmail.com>
2021-07-04 10:39:05 +0530

    addint script for heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 const staticPath = path.join(__dirname, "../public");
 
 app.use(express.static(staticPath));
